7371fbaedc00f3206802131e4c61fdfc3a4841e7
mikeill <mike@mzoo.org>
2025-10-21 19:01:41 -0500

    Three working subclassed models.

--- tracklist/main.py
+++ tracklist/main.py
@@ -1,4 +1,5 @@
 from typing import Union, Annotated
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Query, Depends, HTTPException
 from pydantic import BaseModel
@@ -6,7 +7,13 @@ from datetime import datetime
 import models
 from sqlmodel import Session, select
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    models.create_db_and_tables()
+    yield
+    # Can add events for after startup completion here
+
+app = FastAPI(lifespan=lifespan)
 
 
 class Hymn(BaseModel):
@@ -18,33 +25,31 @@ def get_session():
     with Session(models.engine) as session:
         yield session
 
-
 SessionDep = Annotated[Session, Depends(get_session)]
 
-@app.post("/songs/")
-def create_song(song: models.Song, session: SessionDep) -> models.Song:
-    session.add(song)
+@app.post("/songs/", response_model=models.SongPublic)
+def create_song(song: models.SongCreate, session: SessionDep) -> models.Song:
+    db_song = models.Song.model_validate(song)
+    session.add(db_song)
     session.commit()
-    session.refresh(song)
-    return song
+    session.refresh(db_song)
+    return db_song
 
-@app.get("/songs/")
+@app.get("/songs/", response_model=list[models.SongPublic])
 def read_songs(
     session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
-) -> list[models.Song]:
+):
     songs = session.exec(select(models.Song).offset(offset).limit(limit)).all()
     return songs
 
-
-@app.get("/songs/{song_id}")
+@app.get("/songs/{song_id}", response_model=list[models.SongPublic])
 def read_song(song_id: int, session: SessionDep) -> models.Song:
     song = session.get(models.Song, song_id)
     if not song:
         raise HTTPException(status_code=404, detail="models.Song not found")
     return song
-
 
 @app.delete("/songs/{song_id}")
 def delete_song(song_id: int, session: SessionDep):
@@ -55,16 +60,15 @@ def delete_song(song_id: int, session: SessionDep):
     session.commit()
     return {"ok": True}
 
-
-@app.put("/songs/{song_id}")
-def update_song(song_id: int, session: SessionDep) -> models.Song:
-    song = session.get(models.Song, song_id)
-    if not song:
-        raise HTTPException(status_code=404, detail="models.Song not found")
-    session.update(song)
+@app.patch("/songs/{song_id}", response_model=models.SongPublic)
+def update_song(song_id: int, session: SessionDep, song: models.Song) -> models.Song:
+    song_db = session.get(models.Song, song_id)
+    song_data = song.model_dump(exclude_unset=True)
+    song_db.sqlmodel_update(song_data)
+    session.add(song_db)
     session.commit()
-    session.refresh(song)
-
+    session.refresh(song_db)
+    return song_db
 
 @app.get("/")
 def read_root():
--- tracklist/models.py
+++ tracklist/models.py
@@ -11,22 +11,49 @@ engine = create_engine(settings.db_connection_string, connect_args=connect_args,
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-class Event(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class EventBase(SQLModel):
     date: datetime = Field()
     title: str = Field(default="To be determined", title="The name or title of the event", )
     description: str = Field()
     presenter: str = Field(default=None, title="Is there someone associated with this event", )
 
-class Song(SQLModel, table=True):
+class Event(EventBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    title: str = Field(default="Untitled", title="The title of the piece", )
+
+class EventPublic(EventBase):
+    id: int
+
+class EventCreate(EventBase):
+    date: datetime = Field()
+    title: str = Field(default="To be determined", title="The name or title of the event", )
+    description: str = Field()
+    presenter: str = Field(default=None, title="Is there someone associated with this event", )
+
+class EventUpdate(EventBase):
+    date: datetime = Field()
+    title: str = Field(default="To be determined", title="The name or title of the event", )
+    description: str = Field()
+    presenter: str = Field(default=None, title="Is there someone associated with this event", )
+
+class SongBase(SQLModel):
+    title: str = Field(default="Untitled", title="The title of the piece", index=True)
     credits: str = Field(default="Anonymous", title="Author(s), composer(s), etc...", )
+
+class Song(SongBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+class SongPublic(SongBase):
+    id: int
+
+class SongCreate(SongBase):
+    pass
+
+class SongUpdate(SongBase):
+    title: str | None = None
+    credits: str | None = None
 
 class SongUse(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     event: int = Field(ForeignKey(Event.id), title="When used?")
     song: int = Field(ForeignKey(Song.id), title="Which piece?")
     usage: str = Field(default=None, title="Intro, Offertory, Meditation, etc...", )
-
-create_db_and_tables()
