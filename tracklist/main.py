47cb61ba1c58fac85516d8c9b328398770b0ea06
mikeill <mike@mzoo.org>
2025-10-24 12:04:54 -0500

    Return a list of songs to websocket js module.

--- tracklist/main.py
+++ tracklist/main.py
@@ -1,10 +1,9 @@
-from typing import Union, Annotated
+from typing import Annotated
 
 from fastapi import FastAPI, Query, Depends, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 from datetime import datetime
 from sqlmodel import Session, select
 
@@ -34,10 +33,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-class Hymn(BaseModel):
-    title: str
-    service_date: datetime
-    is_offer: Union[bool, None] = None
 
 def get_session():
     with Session(models.engine) as session:
--- tracklist/songs.py
+++ tracklist/songs.py
@@ -1,4 +1,20 @@
-print("Hi from songs")
+from typing import Annotated
+import json
 
-def get_state(group: str):
-    return {"hello": "world we have songs."}
+from fastapi import Depends, Query
+from pydantic import BaseModel
+from sqlmodel import Session, select
+
+from . import models
+
+
+
+def get_state(
+        group: str,
+        offset: int = 0,
+        limit: Annotated[int, Query(le=100)] = 100,
+    ):
+    with Session(models.engine) as session:
+        songs = session.exec(select(models.Song).offset(offset).limit(limit)).all()
+        songs = [song.model_dump_json() for song in songs]
+    return {"songs":songs}
