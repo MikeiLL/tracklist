08a0a696704af3919f15dc253ef7e0792ddc25a5
mikeill <mike@mzoo.org>
2025-10-27 10:49:04 -0500

    Try joining events and songs.

--- tracklist/songs.py
+++ tracklist/songs.py
@@ -15,6 +15,10 @@ def get_state(
     with Session(models.engine) as session:
         songs = session.exec(select(models.Song).order_by(models.Song.id.desc()).limit(100)).all()
         songs = [song.model_dump() for song in songs]
-        events = session.exec(select(models.Event).order_by(models.Event.date.desc()).limit(100)).all()
+        # select e.date, u.song_id, s.title from event e join songuse u on u.event_id = e.id join song s on s.id = u.song_id;
+        #db.query(Post).join(User).filter(User.id == user_id).all()
+        events = session.exec(select(models.Event)
+                              .join(models.SongUse, isouter=True).filter(models.Event.id == models.SongUse.event_id)
+                              .order_by(models.Event.date.desc()).limit(100)).all()
         events = [event.json() for event in events]
     return {"songs":songs, "events": events}
