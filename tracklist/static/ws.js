1f71b5807f91856c5b2930b1aad0ef91d37a570c
mikeill <mike@mzoo.org>
2025-11-04 11:23:13 -0600

    Retry ws on disconnect.

--- tracklist/static/ws.js
+++ tracklist/static/ws.js
@@ -13,30 +13,34 @@ function getCookie(name) {
 
 
 export default (methods) => {
-    ws = new WebSocket("/ws");
-    ws.onopen = async () => {
-        console.log("websocket connected");
-        let access_token = getCookie("tracklist_access_token");
-        ws.send(JSON.stringify(
-            {
-                "cmd": "init",
-                "type": ws_type,
-                "group": ws_group,
-                "tracklist_access_token": access_token
-            }
-        ));
-        ws.send(JSON.stringify({"cmd": "addsong", "details": {"title": "test song"}}));
+    function reconnect() {
+        ws = new WebSocket("/ws");
+        ws.onopen = async () => {
+            console.log("websocket connected");
+            let access_token = getCookie("tracklist_access_token");
+            ws.send(JSON.stringify(
+                {
+                    "cmd": "init",
+                    "type": ws_type,
+                    "group": ws_group,
+                    "tracklist_access_token": access_token
+                }
+            ));
+            ws.send(JSON.stringify({"cmd": "addsong", "details": {"title": "test song"}}));
+        }
+        ws.onmessage = (e) => {
+            let data = JSON.parse(e.data)
+            if (data.cmd === "update") return methods.render(data);
+            console.log(data);
+        }
+        ws.onclose = async () => {
+            console.log("websocket disconnected");
+            ws = null;
+            setTimeout(reconnect, 250); // try again in 1/4 second
+            // TODO put something in the DOM
+        }
     }
-    ws.onmessage = (e) => {
-        let data = JSON.parse(e.data)
-        if (data.cmd === "update") return methods.render(data);
-        console.log(data);
-    }
-    ws.onclose = async () => {
-        console.log("websocket disconnected");
-        ws = null;
-        // TODO put something in the DOM
-    }
+    reconnect();
     return {
         send: msg => {
             if (ws) {
