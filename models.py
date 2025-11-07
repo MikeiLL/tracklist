3d5d602e9b9cea82476283fce798330823f9c9bc
mikeill <mike@mzoo.org>
2025-11-06 22:00:34 -0600

    Remove some "not null" from song table.

--- models.py
+++ models.py
@@ -48,9 +48,9 @@ class EventUpdate(EventBase):
 
 class SongBase(SQLModel):
     title: str = Field(default="Untitled", title="The title of the piece", index=True)
-    credits: str = Field(default="Anonymous", title="Author(s), composer(s), etc...", )
-    song_number: int = Field(default=None, title="1, 101, 1024, etc...", )
-    notes: str = Field(default="", title="Arbitrary notes", )
+    credits: str | None = Field(default="Anonymous", title="Author(s), composer(s), etc...", )
+    song_number: int | None = Field(default=None, title="1, 101, 1024, etc...", )
+    notes: str | None = Field(default="", title="Arbitrary notes", )
 
 class Song(SongBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
