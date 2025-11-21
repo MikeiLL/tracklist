bbb096d27837af07c6cc0d2e2be4b134fa6b09fd
mikeill <mike@mzoo.org>
2025-11-21 11:36:36 -0600

    Revert "Stop retrying websocket reconnect after 5 seconds and redirect to login screen."
    This reverts commit f303a3f1bb862138b54bff20e8d89c58c5732762.

--- static/ws.js
+++ static/ws.js
@@ -1,4 +1,6 @@
 let ws = null;
+let reconnect_delay = 250;
+const max_reconnect_time = 1000;
 
 function getCookie(name) {
     const cookies = document.cookie.split(";");
@@ -19,6 +21,9 @@ export default (methods) => {
         url.protocol = url.protocol === "https:" ? "wss:" : "ws:"
         ws = new WebSocket(url)
         ws.onopen = async () => {
+            if (reconnect_delay > max_reconnect_time) {
+                return console.log("stop trying")
+            }
             console.log("websocket connected");
             connected = true;
             let access_token = getCookie("tracklist_access_token");
@@ -30,7 +35,7 @@ export default (methods) => {
                     "tracklist_access_token": access_token
                 }
             ));
-            ws.send(JSON.stringify({"cmd": "addsong", "details": {"title": "test song"}}));
+            //ws.send(JSON.stringify({"cmd": "addsong", "details": {"title": "test song"}}));
         }
         ws.onmessage = (e) => {
             let data = JSON.parse(e.data)
@@ -39,10 +44,16 @@ export default (methods) => {
             console.log(data);
         }
         ws.onclose = async () => {
-            console.log("websocket disconnected");
+            console.log("websocket disconnected", reconnect_delay);
             connected = false;
             ws = null;
-            setTimeout(reconnect, 250); // try again in 1/4 second
+            if (reconnect_delay > max_reconnect_time) {
+                setTimeout(reconnect, reconnect_delay);
+                if (reconnect_delay < max_reconnect_time) reconnect_delay *= 1.5 + 0.5 * Math.random(); //Exponential back-off with a (small) random base
+            } else {
+                window.location = "/login"; // take 'em back to index which will return login if no cookie.
+            }
+
             // TODO put something in the DOM
         }
     }
