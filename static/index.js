12b28d44b262e9f4da397cea0cfe6c45ede20e64
mikeill <mike@mzoo.org>
2025-11-25 09:51:01 -0600

    Replace css list marker. Doesn't work on IOS.

--- static/index.js
+++ static/index.js
@@ -14,7 +14,10 @@ const sock = ws({
             state.events && DIV([
                 H2("Upcoming Events"),
                 A({href: "/event", title:"see all events"}, "See all ->"),
-                UL({id: "events", class: "eventlist"}, Object.values(state.events).map(e => LI({"data-date": utils.formatdate(e.date)}, DIV({class:"card"},[
+                UL({id: "events", class: "eventlist"}, Object.values(state.events).map(e => LI(
+                    {"data-date": utils.formatdate(e.date)}, [
+                    DIV({class: "date"}, utils.formatdate(e.date)),
+                    DIV({class: "card"}, [
                     H2([e.title, A({
                         href: `/event/${e.id}`,
                         title: "View or edit event."
@@ -23,11 +26,12 @@ const sock = ws({
                         SPAN({class:"label"}, "Presenter "), e.presenter || "not set", " – ",
                         SPAN({class:"label"}, "Service Leader "), e.contact || "not set",
                     ]),
-                    UL({class: "eventsongs"}, e.songs.map(s => LI({"data-number": s.song_number || ""}, [
+                    UL({class: "eventsongs"}, e.songs.map(s => LI([
+                        SPAN({class: "songnum",}, [s.song_number ? ["#",s.song_number] : ""]),
                         s.title,
                         s.usage && SPAN({style: "color:var(--grey);",}, " ("+s.usage+")"),
                     ])))
-                    ])
+                    ])]
                 ))), // end UL
                 BUTTON({id: "newevent", type: "button"}, "Create Event"),
             ]),
