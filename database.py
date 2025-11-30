3cba5c7b6dac47e4382c9a13d584b11451b1ae16
mikeill <mike@mzoo.org>
2025-11-30 15:44:08 -0600

    Try subclassing ThreadedConnectionPool with Semaphore solution.

--- database.py
+++ database.py
@@ -2,12 +2,33 @@ import psycopg2
 import psycopg2.extras
 import os
 from . import utils
-from psycopg2 import pool
+from psycopg2.pool import ThreadedConnectionPool as _ThreadedConnectionPool
+from threading import Semaphore
 
 DB_CONNECTION_STRING = os.environ["DB_CONNECTION_STRING"]
 
+# source https://stackoverflow.com/a/53437049/2223106
+class ThreadedConnectionPool(_ThreadedConnectionPool):
+    def __init__(self, minconn, maxconn, *args, **kwargs):
+        self._semaphore = Semaphore(maxconn)
+        super().__init__(minconn, maxconn, *args, **kwargs)
+
+    def getconn(self, *args, **kwargs):
+        self._semaphore.acquire()
+        try:
+            return super().getconn(*args, **kwargs)
+        except:
+            self._semaphore.release()
+            raise
+
+    def putconn(self, *args, **kwargs):
+        try:
+            super().putconn(*args, **kwargs)
+        finally:
+            self._semaphore.release()
+
 # Create a connection pool
-connection_pool = pool.SimpleConnectionPool(
+connection_pool = ThreadedConnectionPool(
     1, 10, DB_CONNECTION_STRING
 )
 
